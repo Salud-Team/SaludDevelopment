b34630159629ff1a988fe90a6d37b01d4ee0f496
Widchard Faustin <mrrichfaustin@gmail.com>
2021-03-26 21:20:14 -0400

    Smaller pushes

---
+++ angular-salud-app/src/app/shared/crud.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { retry, catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+
+  endpoint = 'http://localhost:8080/SaludUserData';
+
+  constructor() { }
+}
